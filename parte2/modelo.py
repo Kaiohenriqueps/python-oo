804606095698b4197a7b1dbc9a85d1e8ae1bbd3d
Kaiohenriqueps <kh.silva@hotmail.com>
2021-06-04 11:57:57 -0300

    herança + polimorfismo + representação textual de objetos

--- parte2/modelo.py
+++ parte2/modelo.py
@@ -1,24 +1,54 @@
-from typing import Annotated
-
-
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self._likes} Likes"
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes"
 
-class Serie:
-    def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
-        self.temporadas = temporadas
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temps):
+        super().__init__(nome, ano)
+        self.temps = temps
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temps} temps - {self._likes} Likes"
 
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-print(
-    f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}"
-)
+vingadores.dar_like()
 
 atlanta = Serie("atlanta", 2018, 2)
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}")
+atlanta.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
