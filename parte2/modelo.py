f1102d5cc12109592b05fc41e2474383cda58844
Kaiohenriqueps <kh.silva@hotmail.com>
2021-06-04 13:38:06 -0300

    criando a classe playlist como um iterable

--- parte2/modelo.py
+++ parte2/modelo.py
@@ -41,14 +41,37 @@ class Serie(Programa):
         return f"{self._nome} - {self.ano} - {self.temps} temps - {self._likes} Likes"
 
 
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie("atlanta", 2018, 2)
+tmep = Filme("todo mundo em panico", 1999, 100)
+demolidor = Serie("demolidor", 2016, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+print(f"Tamanho do playlist: {len(playlist_fim_de_semana)}")
 
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f"Tá ou não tá? {demolidor in playlist_fim_de_semana}")
